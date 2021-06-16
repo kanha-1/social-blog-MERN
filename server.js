11941864ab98e95f897cf5284895ce9926ef72e2
kanha_1 <kanhu.sahoo09@gmail.com>
2021-06-16 23:51:11 +0530

    e

--- server.js
+++ server.js
@@ -33,6 +33,9 @@ app.use(usersRoute)
 // heroku deploy
 if(process.env.NODE_ENV == "production"){
 	app.use(express.static("client/build"))
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'client','build',"index.html"))
+	})
 }
 //Server
 app.listen(PORT, function () {
